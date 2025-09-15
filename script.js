bc8414af2f43c114e6af4c229dec89bf48055df7
Alan <161715146+AlanPierree@users.noreply.github.com>
2025-09-15 12:21:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function tocarSom(id) {
 
+    const audios = document.querySelectorAll('audio');
+    
+    audios.forEach(audio => {
+    
+    audio.pause();
+    
+    audio.currentTime = 0;
+    
+    });
+    
+    const audioSelecionado = document.getElementById(id);
+
+    if (audioSelecionado) {
+
+        audioSelecionado.play();
+        
+    }
+    
+    }
